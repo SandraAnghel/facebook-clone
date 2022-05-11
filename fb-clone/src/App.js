d91f7d70237098160363edabb80ecd5df137e4e3
sandraanghel <1815743@student.uwtsd.ac.uk>
2022-05-11 12:17:57 +0100

    Init commit

---
+++ fb-clone/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    // BEM naming convention
+    <div className="app">
+      <h1>Lets build the facebook clone!</h1>
+    </div>
+  );
+}
+
+export default App;
