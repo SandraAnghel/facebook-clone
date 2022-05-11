54ddff6699c25c56b318098a7f390fddf3333178
sandraanghel <1815743@student.uwtsd.ac.uk>
2022-05-11 15:42:39 +0100

    Create header component

---
+++ fb-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDKVblWr8AUQGoYXbIzObwizsI6RSctA0c",
+    authDomain: "facebook-clone-342a0.firebaseapp.com",
+    projectId: "facebook-clone-342a0",
+    storageBucket: "facebook-clone-342a0.appspot.com",
+    messagingSenderId: "349149088816",
+    appId: "1:349149088816:web:976a73bbc3ed632a96b1f5",
+    measurementId: "G-3N9H3LK118"
+  };
