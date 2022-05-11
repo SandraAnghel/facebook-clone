c384be3ad396e16ba472d242c1ad061aa11c41ea
sandraanghel <1815743@student.uwtsd.ac.uk>
2022-05-11 19:12:50 +0100

    Create sidebar and feed components

--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
+import Feed from './Feed';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
@@ -7,10 +9,14 @@ function App() {
     <div className="app">
       <Header />
 
+      <div className='app__body'>
+        <Sidebar />
+        <Feed />
+        {/* Widgets */}
+      </div>
+
       {/* App body */}
-      {/* Sidebar */}
-      {/* Feed */}
-      {/* Widgets */}
+
     </div>
   );
 }
